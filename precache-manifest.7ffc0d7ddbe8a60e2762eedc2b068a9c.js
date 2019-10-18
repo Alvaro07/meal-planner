d0d29b9c132c5f8e18faffaca2bb032d5476057e
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-10-18 11:21:24 +0200

    Updates

---
+++ precache-manifest.7ffc0d7ddbe8a60e2762eedc2b068a9c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b24f38bff2c6c6fd1c6b8afd5575676a",
+    "url": "/meal-planner/index.html"
+  },
+  {
+    "revision": "d5fce065e068784c29c3",
+    "url": "/meal-planner/static/js/2.e401a701.chunk.js"
+  },
+  {
+    "revision": "3ab0c053e4eca7735f67",
+    "url": "/meal-planner/static/js/main.fc766eea.chunk.js"
+  },
+  {
+    "revision": "8252df5f0a8d22ec8e06",
+    "url": "/meal-planner/static/js/runtime-main.883e8dad.js"
+  }
+]);
